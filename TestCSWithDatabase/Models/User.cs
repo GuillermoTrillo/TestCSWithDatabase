299748d4d1fcb770cd81064e7a0e175ffb0f85c4
Guillermo del Toro Trillo <guillermotoro61@gmail.com>
2024-10-18 11:42:41 -0300

    asd

--- TestCSWithDatabase/Models/User.cs
+++ TestCSWithDatabase/Models/User.cs
@@ -5,5 +5,14 @@
         int id;
         string name;
         string password;
+
+        public string getName()
+        {
+            return name; 
+        }
+        public void setName(string newName)
+        {
+            name = newName;
+        }
     }
 }
--- TestCSWithDatabase/Services/UserDAO.cs
+++ TestCSWithDatabase/Services/UserDAO.cs
@@ -1,13 +1,20 @@
-﻿namespace TestCSWithDatabase.Services
+﻿using System.Data.SqlClient;
+using TestCSWithDatabase.Models;
+
+namespace TestCSWithDatabase.Services
 {
     public class UserDAO
     {
         string connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
-        public bool FindUserByPassword()
+        public bool FindUserByPassword(User user)
         {
-            bool passwordIsCorrect = false;
-            return passwordIsCorrect;
+            string sqlStatement = "SELECT * FROM dbo.User WHERE password = @password";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sqlStatement, connection);
+                cmd.Parameters.Add("@username", System.Data.SqlDbType.VarChar, 40).Value = User.name;
+            }
         }
     }
 }
